02447c0b40cc13e65b6e54606c667ef4f4a1bed2
ten-1010 <ten-ten0426@outlook.jp>
2023-07-15 17:27:56 +0900

    凡例を追加

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#データの生成  
+#課題１：データの生成
 
 data_length = 100 #データの個数
 # 初期値
@@ -12,5 +12,12 @@ for t in range(data_length-1):
     x_next = data[-1] + n
     data.append(x_next)
 
-plt.plot(range(data_length),data)
+nois = np.random.normal(loc=0,scale=1,size=100)
+y = data + nois
+plt.plot(range(data_length),data,label="x")
+plt.plot(range(data_length),y,label="y")
+plt.xlabel("T")
+plt.legend()
 plt.show()
+
+# 課題2　
